1c6dbc25bee7a7787ef4004b2ea1c2346cd30068
Hendrix Nwaokolo <n.hendrix.e@gmail.com>
2026-08-21 18:36:33 +0100

    Exclude authenticated API routes from browser CSRF

---
+++ magic_login/config/csrf_exclude_uris.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+return [
+    'magic_login/api/create-link',
+    'magic_login/api/request-otp',
+    'magic_login/api/verify-otp',
+    'magic_login/api/revoke',
+];
